d57a5b0e0c670da2aacd7271131f439f369aaa34
lakshmansrikanth123 <lakshmansrikanthkorla.225@gmail.com>
2026-02-23 13:03:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# python
+# Python Jenkins Build Demo
+
+## Steps to use in Jenkins:
+
+1. Create a Pipeline Job
+2. Point to this project
+3. Run Build
+
+## Output:
+python-build-demo.tar.gz package created
---
+++ app.py
@@ -0,0 +1,10 @@
+from calculator import add, subtract, multiply, divide
+
+print("Python Jenkins Build Demo")
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 5))
+
+print("Build Successful")
